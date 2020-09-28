c9ea1443f5563c36bf4d1ad0609c13278196ce88
unknown <ompreetam317@gmail.com>
2020-09-28 19:05:15 +0530

    [Preetam]Added: Feature to validate the abc part of email

--- src/emailValidation/EmailValidator.java
+++ src/emailValidation/EmailValidator.java
@@ -4,15 +4,34 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-	
+	public static void Validator(String s) {
+		
+		 String regex = "^([a-z A-Z 0-9]+)([a-z A-Z 0-9 .!#$%&'*+/=?^_`{|}~-]*)$";
+	     
+	      boolean result = Pattern.matches(regex,s);
+	      if(result) {
+	         System.out.println("Given email is valid");
+	      } else {
+	         System.out.println("Given email is not valid");
+	      }
+	   }
+
 	
 
 		
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome To Email Verification Code");
+		Scanner sc = new Scanner(System.in);
+	      
+	      while(true){
+	    	  System.out.println("Enter your Email: ");
+		      String email = sc.next();
+		      if(email.matches("stop")) {
+		    	  return;
+		      }
+		      Validator(email);
 	    
-	}
+	      }
 	   
 	    
 	    
@@ -22,4 +41,5 @@ public class EmailValidator {
 		
 	
 	
+	}
 }
