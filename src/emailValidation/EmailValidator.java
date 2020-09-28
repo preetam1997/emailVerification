40c534f56abea1f4a9f6df86e5d40d4babf14a04
unknown <ompreetam317@gmail.com>
2020-09-28 19:00:42 +0530

    [Preetam]Added: Welcome Message

---
+++ src/emailValidation/EmailValidator.java
@@ -0,0 +1,25 @@
+package emailValidation;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+	
+	
+
+		
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Email Verification Code");
+	    
+	}
+	   
+	    
+	    
+	
+		
+		
+		
+	
+	
+}
